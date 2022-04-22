bfe2c9c9c6e3764bab0a30b2e27564bfac8e288c
helen-daan <102533298+helen-daan@users.noreply.github.com>
2022-04-22 11:22:17 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -537,3 +537,30 @@ var MD5 = function (string) {
 
     return temp.toLowerCase();
 };
+
+/********* Timeline ******/
+
+const items = document.querySelectorAll("#timeline li");
+
+const isInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWhith || document.documentElement.clientWidth)
+  );
+};
+
+const run = () =>
+  items.forEach((item) => {
+    if (isInViewport(item)) {
+      item.classList.add("show");
+    }
+  });
+
+// Events
+window.addEventListener("load", run);
+window.addEventListener("resize", run);
+window.addEventListener("scroll", run);
